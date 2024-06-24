595c4f64cbc7712a66457b28c0055a36804f779a
Maxim <max.sherb2007@gmail.com>
2024-06-24 22:29:24 +0300

    Initial commit

---
+++ src/main/java/org/astron/tickfocus/TickFocusApplication.java
@@ -0,0 +1,13 @@
+package org.astron.tickfocus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TickFocusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TickFocusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/astron/tickfocus/TickFocusApplicationTests.java
@@ -0,0 +1,13 @@
+package org.astron.tickfocus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TickFocusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
