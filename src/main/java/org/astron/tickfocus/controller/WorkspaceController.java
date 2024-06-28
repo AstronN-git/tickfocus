58244898950ff07001ac99d08d2ab28a2978b803
Maxim <max.sherb2007@gmail.com>
2024-06-28 20:19:23 +0300

    renamed home endpoint and view to workspace

--- src/main/java/org/astron/tickfocus/controller/WorkspaceController.java
+++ src/main/java/org/astron/tickfocus/controller/WorkspaceController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/home")
+@RequestMapping("/workspace")
 public class WorkspaceController {
     @GetMapping
     public String showHomeView() {
-        return "home";
+        return "workspace";
     }
 }
