dcc73768830506e6a05e8d02a902b01bc75537ca
Maxim <max.sherb2007@gmail.com>
2024-06-30 13:18:59 +0300

    introduced spring security, spring data jpa + h2; moved css from /styles to /css

---
+++ src/main/java/org/astron/tickfocus/configuration/SecurityConfiguration.java
@@ -0,0 +1,34 @@
+package org.astron.tickfocus.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toStaticResources;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers(toH2Console()).permitAll()
+                        .requestMatchers(toStaticResources().atCommonLocations()).permitAll()
+                        .requestMatchers("/workspace/**").permitAll()
+                        .anyRequest().denyAll()
+                )
+                .headers((headers) -> headers
+                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)
+                )
+                .csrf((csrf) -> csrf
+                        .ignoringRequestMatchers(toH2Console())
+                );
+
+        return http.build();
+    }
+}
